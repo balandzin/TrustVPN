815402f244435e42c4097e645c422fa902857bc6
balandzin <balandzin2602@gmail.com>
2024-09-25 12:24:39 +0300

    Добавлены изображения onboard

--- TrustVPN/Modules/OnboardScene.swift
+++ TrustVPN/Modules/OnboardScene.swift
@@ -2,7 +2,17 @@ import SnapKit
 import UIKit
 
 final class OnboardScene: UIViewController {
+    
     // MARK: - GUI Variables
+    private let imageStack = UIStackView(ax: .horizontal, alignm: .center, distr: .fill)
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: "onboardImage")
+        return image
+    }()
+    
     
     // MARK: - Properties
     
