411f223b375c2562fd6217ec69b77f2f92e79135
balandzin <balandzin2602@gmail.com>
2024-10-10 17:19:32 +0300

    Добавил описания в тексты

--- TrustVPN/Modules/TabBar/PasswordSecurity/PasswordSecurityController.swift
+++ TrustVPN/Modules/TabBar/PasswordSecurity/PasswordSecurityController.swift
@@ -314,3 +314,331 @@ final class PasswordSecurityController: UIViewController, UITableViewDelegate, U
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//import UIKit
+//
+//final class PasswordSecurityController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//    
+//    // MARK: - GUI Variables
+//    private lazy var headerLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = AppText.passwordSecurity
+//        label.textAlignment = .left
+//        label.textColor = AppColors.almostWhite
+//        label.font = .systemFont(ofSize: 26, weight: .bold)
+//        return label
+//    }()
+//    
+//    private lazy var dataIsSecureStackView: UIStackView = {
+//        let stackView = UIStackView()
+//        stackView.axis = .horizontal
+//        stackView.alignment = .leading
+//        stackView.distribution = .fill
+//        stackView.spacing = 10
+//        return stackView
+//    }()
+//    
+//    private lazy var shieldView: UIImageView = {
+//        let image = UIImageView()
+//        image.image = .loadImage(LoadService.shared.load?.images?.shield) ?? UIImage(named: "shield")
+//        image.contentMode = .scaleAspectFit
+//        return image
+//    }()
+//    
+//    private lazy var dataIsSecureLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = AppText.dataIsSecure
+//        label.textColor = AppColors.dataSecurityLabel
+//        label.adjustsFontSizeToFitWidth = true
+//        label.font = .systemFont(ofSize: 14)
+//        return label
+//    }()
+//    
+//    private lazy var passwordTextField: UITextField = {
+//        let textField = PaddedTextField()
+//        textField.placeholder = AppText.typePassword
+//        textField.placeHolderColor = AppColors.dataSecurityLabel
+//        textField.font = .systemFont(ofSize: 14, weight: .bold)
+//        textField.isSecureTextEntry = true
+//        textField.autocapitalizationType = .none
+//        textField.padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 40)
+//        textField.textColor = AppColors.almostWhite
+//        textField.layer.cornerRadius = 10
+//        textField.keyboardAppearance = .dark
+//        textField.backgroundColor = AppColors.textFieldBackground
+//        return textField
+//    }()
+//    
+//    private lazy var togglePasswordVisibilityButton: UIButton = {
+//        let button = UIButton()
+//        button.setImage(.loadImage(LoadService.shared.load?.images?.eye) ?? UIImage(named: "eye"), for: .normal)
+//        button.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
+//        return button
+//    }()
+//    
+//    private lazy var progressStackView: UIStackView = {
+//        let stackView = UIStackView()
+//        stackView.axis = .vertical
+//        stackView.alignment = .center
+//        stackView.distribution = .fill
+//        stackView.spacing = 13
+//        return stackView
+//    }()
+//    
+//    private lazy var progressBar: UIProgressView = {
+//        let progressView = UIProgressView()
+//        progressView.progress = 0.5
+//        progressView.progressTintColor = AppColors.termsView
+//        progressView.trackTintColor = AppColors.textFieldBackground
+//        return progressView
+//    }()
+//    
+//    private lazy var successStackView: UIStackView = {
+//        let stackView = UIStackView()
+//        stackView.axis = .horizontal
+//        stackView.alignment = .center
+//        stackView.distribution = .fill
+//        stackView.spacing = 10
+//        return stackView
+//    }()
+//    
+//    private lazy var successLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = AppText.unsecurePassword
+//        label.textColor = AppColors.unsecurePassword
+//        label.font = .systemFont(ofSize: 18, weight: .bold)
+//        label.numberOfLines = 0
+//        label.textAlignment = .left
+//        label.adjustsFontSizeToFitWidth = true
+//        return label
+//    }()
+//    private lazy var successImage: UIImageView = {
+//        let image = UIImageView()
+//        image.image = .loadImage(LoadService.shared.load?.images?.unsecurePassword) ?? UIImage(named: "unsecurePassword")
+//        return image
+//    }()
+//    
+//    private lazy var crackedDescriptionLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = AppText.crackedDescriptionLabel
+//        label.textColor = AppColors.almostWhite
+//        label.font = .systemFont(ofSize: 18)
+//        label.numberOfLines = 0
+//        label.textAlignment = .center
+//        label.numberOfLines = 0
+//        return label
+//    }()
+//    
+//    private lazy var tipsLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = AppText.tips
+//        label.textColor = AppColors.almostWhite
+//        label.font = .systemFont(ofSize: 20, weight: .bold)
+//        label.numberOfLines = 0
+//        label.textAlignment = .left
+//        return label
+//    }()
+//    
+//    // Данные для таблицы
+//        let titles = [
+//            "1. Как создать надежный пароль?",
+//            "2. Почему безопасность паролей важна?",
+//            "3. Как работает наш инструмент?",
+//            "4. Типы атак на пароли"
+//        ]
+//        
+//        let contents = [
+//            "Пароль должен содержать как минимум 12 символов. Используйте комбинацию букв, цифр и специальных символов для усиления безопасности.",
+//            "Слабые пароли могут привести к взлому аккаунта и утечке личных данных. Важно использовать сильные и уникальные пароли.",
+//            "Наш инструмент помогает создавать, сохранять и управлять паролями, что повышает вашу безопасность в интернете.Наш инструмент помогает создавать, сохранять и управлять паролями, что повышает вашу безопасность в интернете.Наш инструмент помогает создавать, сохранять и управлять паролями, что повышает вашу безопасность в интернете.",
+//            "Существует множество типов атак, таких как фишинг, атаки методом перебора и социальной инженерии. Узнайте, как защититься от них."
+//        ]
+//        
+//        // Переменные для отслеживания раскрытия секций
+//        var expandedSections = Set<Int>()
+//        
+//        // Таблица
+//        let tableView: UITableView = {
+//            let tableView = UITableView(frame: .zero, style: .grouped)
+//            tableView.translatesAutoresizingMaskIntoConstraints = false
+//            return tableView
+//        }()
+//    
+//
+//    // MARK: - Lifecycle
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        setupStyle()
+//        setupUI()
+//    }
+//   
+//    // MARK: - Actions
+//    @objc private func togglePasswordVisibility() {
+//        passwordTextField.isSecureTextEntry = !passwordTextField.isSecureTextEntry
+//    }
+//    
+//    // MARK: - Private Methods
+//    private func setupStyle() {
+//        view.backgroundColor = .gradientColor
+//        navigationController?.isNavigationBarHidden = true
+//    }
+//    
+//    private func setupUI() {
+//        view.addSubview(headerLabel)
+//        view.addSubview(dataIsSecureStackView)
+//        dataIsSecureStackView.addArrangedSubview(shieldView)
+//        dataIsSecureStackView.addArrangedSubview(dataIsSecureLabel)
+//        view.addSubview(passwordTextField)
+//        passwordTextField.addSubview(togglePasswordVisibilityButton)
+//        view.addSubview(tipsLabel)
+//        view.addSubview(progressStackView)
+//        progressStackView.addArrangedSubview(progressBar)
+//        progressStackView.addArrangedSubview(successStackView)
+//        successStackView.addArrangedSubview(successImage)
+//        successStackView.addArrangedSubview(successLabel)
+//        progressStackView.addArrangedSubview(crackedDescriptionLabel)
+//
+//        setupConstraints()
+//        
+//    }
+//    
+//    private func setupConstraints() {
+//        headerLabel.snp.makeConstraints { make in
+//            make.top.equalTo(view.safeAreaLayoutGuide).offset(15)
+//            make.leading.trailing.equalToSuperview().inset(24)
+//        }
+//        
+//        dataIsSecureStackView.snp.makeConstraints { make in
+//            make.top.equalTo(headerLabel.snp.bottom).offset(30)
+//            make.leading.trailing.equalToSuperview().inset(24)
+//        }
+//        
+//        shieldView.snp.makeConstraints { make in
+//            make.width.height.equalTo(24)
+//        }
+//        
+//        dataIsSecureLabel.snp.makeConstraints { make in
+//            make.leading.equalTo(shieldView.snp.trailing).offset(10)
+//            make.centerY.equalTo(shieldView)
+//        }
+//        
+//        passwordTextField.snp.makeConstraints { make in
+//            make.top.equalTo(dataIsSecureStackView.snp.bottom).offset(20)
+//            make.leading.trailing.equalToSuperview().inset(24)
+//            make.height.equalTo(50)
+//        }
+//        
+//        togglePasswordVisibilityButton.snp.makeConstraints { make in
+//            make.width.height.equalTo(18)
+//            make.trailing.equalTo(passwordTextField.snp.trailing).inset(20)
+//            make.centerY.equalTo(passwordTextField)
+//        }
+//        
+//        progressStackView.snp.makeConstraints { make in
+//            make.top.equalTo(passwordTextField.snp.bottom).offset(12)
+//            make.leading.trailing.equalToSuperview().inset(40)
+//        }
+//        
+//        progressBar.snp.makeConstraints { make in
+//            make.height.equalTo(6)
+//            make.leading.trailing.equalToSuperview()
+//        }
+//        
+//        successImage.snp.makeConstraints { make in
+//            make.width.height.equalTo(24)
+//        }
+//        
+//        tipsLabel.snp.makeConstraints { make in
+//            make.top.equalTo(progressStackView.snp.bottom).offset(30)
+//            make.leading.trailing.equalToSuperview().inset(24)
+//        }
+//        
+//        
+//        // Настройка таблицы
+//                view.addSubview(tableView)
+//        tableView.backgroundColor = .clear
+//                tableView.delegate = self
+//                tableView.dataSource = self
+//                tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//                
+//                // Устанавливаем ограничения для таблицы
+//                NSLayoutConstraint.activate([
+//                    tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//                    tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//                    tableView.topAnchor.constraint(equalTo: tipsLabel.bottomAnchor, constant: 30),
+//                    tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+//                ])
+//    }
+//    
+//    // Количество секций
+//        func numberOfSections(in tableView: UITableView) -> Int {
+//            return titles.count
+//        }
+//        
+//        // Количество строк в секции (одна строка для контента)
+//        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//            return expandedSections.contains(section) ? 1 : 0
+//        }
+//        
+//        // Ячейки с контентом
+//        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//            cell.textLabel?.text = contents[indexPath.section]
+//            cell.textLabel?.numberOfLines = 0 // Делаем текст многострочным
+//            cell.backgroundColor = .clear
+//            cell.textLabel?.textColor = .white
+//            return cell
+//        }
+//        
+//        // Заголовок для каждой секции
+//        func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//            let headerButton = UIButton(type: .system)
+//            headerButton.setTitle(titles[section], for: .normal)
+//            headerButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+//            headerButton.contentHorizontalAlignment = .left
+//            headerButton.backgroundColor = .darkGray
+//            headerButton.setTitleColor(.white, for: .normal)
+//            headerButton.tag = section
+//            headerButton.addTarget(self, action: #selector(didTapHeader(_:)), for: .touchUpInside)
+//            
+//            return headerButton
+//        }
+//        
+//        // Высота заголовка секции
+//        func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//            return 50
+//        }
+//        
+//        // Динамическая высота ячеек
+//        func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//            return UITableView.automaticDimension
+//        }
+//        
+//        // Действие при нажатии на заголовок секции
+//        @objc func didTapHeader(_ sender: UIButton) {
+//            let section = sender.tag
+//            
+//            if expandedSections.contains(section) {
+//                expandedSections.remove(section) // Скрываем секцию, если она раскрыта
+//            } else {
+//                expandedSections.insert(section) // Раскрываем секцию
+//            }
+//            
+//            // Обновляем секцию с анимацией
+//            tableView.reloadSections(IndexSet(integer: section), with: .automatic)
+//        }
+//    
+//    
+//}
--- TrustVPN/Resource/AppText.swift
+++ TrustVPN/Resource/AppText.swift
@@ -42,4 +42,46 @@ final class AppText {
     static var mediumPassword: String = "Medium password"
     static var strongPassword: String = "Strong password"
     static var crackedDescriptionLabel: String = "Your password can be cracked in"
+    static var howToCreateStrongPasswords: String = "How to Create Strong Passwords"
+    static var howToCreateStrongPasswordsDescription: String =
+"""
+If your password strength rating was not satisfactory, it’s time to create new, more secure passwords. Here are some best practices to follow:
+ •  Length Matters: Passwords should be at least 12 characters long, with 16 characters being ideal. Studies show that 45% of Americans use passwords with eight characters or fewer, which are less secure than longer ones.
+ •  Use a Mix of Characters: Include a combination of uppercase and lowercase letters, numbers, and special characters in your passwords.
+ •  Unique Passwords for Each Account: Avoid using the same password across multiple accounts. Each online account should have its own unique password.
+ •  Avoid Personal Information: Do not use easily accessible personal information, such as your birthday, address, or names of children or pets, as these can be exploited in identity theft or data breaches.
+ •  No Consecutive Patterns: Steer clear of using consecutive letters or numbers like "ABCD" or "1234" in your passwords.
+ •  Avoid Common Passwords: Never use "password" or simple repetitive patterns like "aaaa" or "1111" as your password.
+"""
+    static var whyIsPasswordSecurityImportant: String = "Why is password security important?"
+    static var whyIsPasswordSecurityImportantDescription: String = 
+"""
+Inadequate password security can lead to serious consequences, including but not limited to:
+ •  Data Theft: Hackers who gain access to a user’s credentials can log into their accounts to steal personally identifiable information (PII), such as names, addresses, and bank account details. This information can be used to steal money directly or to commit identity theft, which may result in further financial losses or complications in securing loans and employment.
+ •  Privacy Invasion: Weak passwords can compromise personal privacy. For instance, failing to change the default password on IP security cameras could allow hackers to access your account and monitor activities inside your home.
+ •  Corporate Risks: For businesses, weak password security can lead to breaches where hackers launch disinformation campaigns, leak sensitive data to competitors, or demand ransom for stolen information.
+"""
+    
+    static var howOurToolWorks: String = "How our tool works?"
+    static var howOurToolWorksDescription: String =
+"""
+The Password Protection tool checks user passwords using a special algorithm that calculates password strength based on the characters entered. Keep the following guidelines in mind when creating passwords:
+
+Length: A password should contain at least eight to ten characters, the ideal length is 16-20 characters.
+Character combination: A strong password should contain not just a phrase, but a combination of letters, numbers, and symbols. Each character type has its own numerical value, which is summed up to get a total score.
+Uniqueness: The password must not contain repeating characters, instead it must use unique combinations.
+
+Using these criteria, the tool calculates a score for each password, which is then translated into the estimated time it would take a computer to crack the password. For example, a password such as “f0JB^B5sjmXl” would take approximately 34 years to crack.
+"""
+    static var typesOfPasswordAttacks: String = "Types of password attacks"
+    static var typesOfPasswordAttacksDescription: String = 
+"""
+There are several main types of password attacks, with the most common being:
+ 1.  Brute Force Attack: This technique involves trying all possible combinations of characters to crack a password. For example, if a password is two characters long, the attack will try combinations in the following order: "aa", "ab", "ac", and so on, until all possible variations are exhausted. This method requires significant computational resources and time, especially for long and complex passwords, but it guarantees that the password will be cracked if enough time and resources are available.
+ 2.  Dictionary Attack: Unlike brute force attacks, this method uses a precompiled list of words (dictionaries) to find matches with the password. Such attacks use common passwords, phrases, and known word combinations like "password", "123456", "qwerty", and their variations. This method is faster than brute force as it targets more likely options.
+ 3.  Rainbow Table Attack: This technique uses precomputed hash values and their corresponding passwords. Hash functions convert passwords into encrypted strings, and if a password's hash matches a hash in the table, the password is considered cracked. This method is effective for cracking weak and reused passwords.
+ 4.  Social Engineering: This type of attack relies on manipulating people to obtain passwords and other confidential information. It can include phishing, deception, or impersonation to trick the victim into revealing their passwords or personal data.
+ 5.  Phishing: The attacker creates fake websites or sends fraudulent emails to deceive users into entering their passwords. This data is then used to access accounts.
+ 6.  Credential Stuffing: This method uses already compromised usernames and passwords to attempt to log into other accounts. This approach is effective against users who use the same password across multiple services.
+"""
 }
